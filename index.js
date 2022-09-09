9b7445c632f9a52ccb9ea93574bf229cb015b3d9
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-09-09 15:59:19 +0300

    chang2

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(require("./routes/students.route"));
 const port = 3000;
-mongoose
-  .connect("mongodb+srv://Mansur:1954@cluster0.xyb0huh.mongodb.net/test", {
+
+mongoose.connect("mongodb+srv://Mansur:1954@cluster0.xyb0huh.mongodb.net/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
   .catch(() => console.log("Ошибка при соединении с сервером MongoDB")); //
+
 app.listen(port, function (req, res) {
   console.log("Сервер запущен.");
 });
